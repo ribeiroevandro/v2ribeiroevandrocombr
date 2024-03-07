fd5719ee5300f99a875a09be43020e7d920ca1df
Evandro Ribeiro <ribeiroevandro@live.com>
2024-03-07 01:00:39 -0300

    feat: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,19 @@
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
-      <div className="max-w-6xl mx-auto px-5 p-10">
-        <h3 className="font-semibold text-2xl mb-10 lg:mb-0 lg:pr-4">
-          Sample website built with{' '}
-          <a className="underline" href="https://outstatic.com/">
-            Outstatic
-          </a>{' '}
-          and{' '}
-          <a className="underline" href="https://nextjs.org/">
-            Next.js
+      <div className="flex flex-col gap-2 sm:flex-row justify-center items-center max-w-6xl container mx-auto p-10">
+        <p className="text-xs font-semibold">
+          © {new Date().getFullYear()} Evandro Ribeiro. Todos os direitos
+          reservados
+        </p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <a
+            className="text-xs hover:underline underline-offset-4"
+            href="https://linkedin.com/in/ribeiroevandro"
+          >
+            Desenvolvido por: <strong>Evandro Ribeiro</strong>
           </a>
-        </h3>
+        </nav>
       </div>
     </footer>
   )
